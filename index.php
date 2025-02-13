c636921a2f547e8acf1cc045c560e0ac1cfa4bda
ShukSavvy <161610111+ShukSavvy@users.noreply.github.com>
2025-02-13 05:35:22 -0800

    wip b3

---
+++ index.php
@@ -0,0 +1,302 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Font Awesome  -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+  <link rel="stylesheet" href="https://use.fontawesome.com/release/v5.7.2/css/all.css" crossorigin="anonymous">
+
+  <!-- Custom Css-->
+  <link rel="stylesheet" href="./assets/css/style.css">
+
+  <!-- Google fonts -->
+  <link href="https://fonts.googleapis.com/css2?family=Candal&family=Lora:ital,wght@0,400..700;1,400..700&display=swap"
+    rel="stylesheet">
+
+  <title>Blog</title>
+</head>
+
+<body>
+  <header>
+    <div class="logo">
+      <h1 class="logo-text"><span>Dev</span>Shuk</h1>
+    </div>
+    <i class="fa fa-bars menu-toggle"></i>
+    <ul class="nav">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Services</a></li>
+      <!-- <li><a href="#">Sign up</a></li>
+      <li><a href="#">Login</a></li> -->
+      <li>
+        <a href="#">
+          <i class="fa fa-user"></i>
+          Dev Shuk
+          <i class="fa fa-chevron-down" style="font-size: .8em;"></i>
+        </a>
+        <ul>
+          <li><a href="#">Dashboard</a></li>
+          <li><a href="#" class="logout">Logout</a></li>
+        </ul>
+      </li>
+    </ul>
+  </header>
+
+  <!-- Page Wrapper -->
+  <div class="page-wrapper">
+
+  <!-- post Slider -->
+  <div class="post-slider">
+    <h1 class="slider-title">Trending Posts</h1>
+    <i class="fas fa-chevron-left prev"></i>
+    <i class="fas fa-chevron-right next"></i>
+
+      <div class="post-wrapper">
+        <div class="post">
+          <img src="./assets/img/2.jpg" ait="" class="slider-image">
+          <div class="post-info">
+            <h4><a href="single.php">One day your life will flash before your eyes</a></h4>
+            <i class="far fa-user">DevShuk</i>
+            &nbsp;
+            <i class="far fa-calender">Feb 10, 2025</i>
+          </div>
+        </div>
+      
+        <div class="post">
+          <img src="./assets/img/2.jpg" ait="" class="slider-image">
+          <div class="post-info">
+            <h4><a href="single.php">One day your life will flash before your eyes</a></h4>
+            <i class="far fa-user">DevShuk</i>
+            &nbsp;
+            <i class="far fa-calender">Feb 10, 2025</i>
+          </div>
+        </div>
+    
+        <div class="post">
+          <img src="./assets/img/2.jpg" ait="" class="slider-image">
+          <div class="post-info">
+            <h4><a href="single.php">One day your life will flash before your eyes</a></h4>
+            <i class="far fa-user">DevShuk</i>
+            &nbsp;
+            <i class="far fa-calender">Feb 10, 2025</i>
+          </div>
+        </div>
+    
+      
+        <div class="post">
+          <img src="./assets/img/2.jpg" ait="" class="slider-image">
+          <div class="post-info">
+            <h4><a href="single.php">One day your life will flash before your eyes</a></h4>
+            <i class="far fa-user">DevShuk</i>
+            &nbsp;
+            <i class="far fa-calender">Feb 10, 2025</i>
+          </div>
+        </div>
+      
+        <div class="post">
+          <img src="./assets/img/2.jpg" ait="" class="slider-image">
+          <div class="post-info">
+            <h4><a href="single.php">One day your life will flash before your eyes</a></h4>
+            <i class="far fa-user">DevShuk</i>
+            &nbsp;
+            <i class="far fa-calender">Feb 10, 2025</i>
+          </div>
+        </div>
+      
+        <div class="post">
+          <img src="./assets/img/2.jpg" ait="" class="slider-image">
+          <div class="post-info">
+            <h4><a href="single.php">One day your life will flash before your eyes</a></h4>
+            <i class="far fa-user">DevShuk</i>
+            &nbsp;
+            <i class="far fa-calender">Feb 10, 2025</i>
+          </div>
+        </div>
+     
+        <div class="post">
+          <img src="./assets/img/2.jpg" ait="" class="slider-image">
+          <div class="post-info">
+            <h4><a href="single.php">One day your life will flash before your eyes</a></h4>
+            <i class="far fa-user">DevShuk</i>
+            &nbsp;
+            <i class="far fa-calender">Feb 10, 2025</i>
+          </div>
+        </div> 
+      
+        <div class="post">
+          <img src="./assets/img/2.jpg" ait="" class="slider-image">
+          <div class="post-info">
+            <h4><a href="single.php">One day your life will flash before your eyes</a></h4>
+            <i class="far fa-user">DevShuk</i>
+            &nbsp;
+            <i class="far fa-calender">Feb 10, 2025</i>
+          </div>
+        </div>
+     
+   
+        <div class="post">
+          <img src="./assets/img/2.jpg" ait="" class="slider-image">
+          <div class="post-info">
+            <h4><a href="single.php">One day your life will flash before your eyes</a></h4>
+            <i class="far fa-user">DevShuk</i>
+            &nbsp;
+            <i class="far fa-calender">Feb 10, 2025</i>
+          </div>
+        </div>
+      
+        <div class="post">
+          <img src="./assets/img/2.jpg" ait="" class="slider-image">
+          <div class="post-info">
+            <h4><a href="single.php">One day your life will flash before your eyes</a></h4>
+            <i class="far fa-user ">DevShuk</i>
+          &nbsp;
+            <i class="far fa-calender">Feb 10, 2025</i>
+          </div>
+        </div>
+      
+      
+    </div>
+  </div>
+  <!-- End post Slider -->
+
+  <!-- Content -->
+ <div class="content clearfix">
+
+ <!-- Main Content -->
+  <div class="main-content">
+    <h2 class="recent-post-title">Recent Posts</h2>
+    <div class="post clearfix">
+      <img src="./assets/img/1.jpg" alt="" class="post-image">
+      <div class="post-preview">
+        <h1><a href="single.php">The stongest and sweetest songs yet remain to be sung</a></h1>
+        <i class="far fa-user">DevShuk</i>
+        &nbsp;
+        <i class="far calender">Feb 10, 2025</i>
+        <p class="preview-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae esse libero eveniet optio quisquam.</p>
+        <a href="single.php" class="btn read-more">Read More</a>
+      </div>
+    </div>
+    <div class="post clearfix">
+      <img src="./assets/img/4.jpg" alt="" class="post-image">
+      <div class="post-preview">
+        <h1><a href="single.php">The stongest and sweetest songs yet remain to be sung</a></h1>
+        <i class="far fa-user">DevShuk</i>
+        &nbsp;
+        <i class="far calender">Feb 10, 2025</i>
+        <p class="preview-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae esse libero eveniet optio quisquam.</p>
+        <a href="single.php" class="btn read-more">Read More</a>
+      </div>
+    </div>
+    <div class="post clearfix">
+      <img src="./assets/img/4.jpg" alt="" class="post-image">
+      <div class="post-preview">
+        <h1><a href="single.php">The stongest and sweetest songs yet remain to be sung</a></h1>
+        <i class="far fa-user">DevShuk</i>
+        &nbsp;
+        <i class="far calender">Feb 10, 2025</i>
+        <p class="preview-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae esse libero eveniet optio quisquam.</p>
+        <a href="single.php" class="btn read-more">Read More</a>
+      </div>
+    </div>
+    <div class="post clearfix">
+      <img src="./assets/img/1.jpg" alt="" class="post-image">
+      <div class="post-preview">
+        <h1><a href="single.php">The stongest and sweetest songs yet remain to be sung</a></h1>
+        <i class="far fa-user">DevShuk</i>
+        &nbsp;
+        <i class="far calender">Feb 10, 2025</i>
+        <p class="preview-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae esse libero eveniet optio quisquam.</p>
+        <a href="single.html" class="btn read-more">Read More</a>
+      </div>
+    </div>
+
+  </div>
+  <div class="sidebar">
+    <div class="section search">
+    <h2 class="section-title">Search</h2>
+    <form action="">
+      <input type="text" name="search-term" class="text-input" placeholder=Search......">
+    </form>
+    </div>
+
+    <div class="section topics">
+      <h2 class="section-title">Topicas</h2>
+      <ul>
+        <li><a href="#">Poems</a></li>
+        <li><a href="#">Qoutes</a></li>
+        <li><a href="#">Fiction</a></li>
+        <li><a href="#">Biography</a></li>
+        <li><a href="#">Motivation</a></li>
+        <li><a href="#">Inspiration</a></li>
+        <li><a href="#">Life Lessons</a></li>
+      </ul>
+    </div>
+
+  </div>
+ </div>
+  <!--// Content -->
+
+  </div>
+  <!-- end of page-wrapper -->
+
+  <!-- footer -->
+    <div class="footer">
+      <div class="footer-content">
+        <div class="footer-section about">
+          <h1 class="logo-text"><span>Dev</span>Shuk</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus optio possimus iure ex molestiae nihil vero, rerum, dolorum nobis.</p>
+          <div class="contact">
+            <span><i class="fas fa-phone"></i> &nbsp; 7066896092</span>
+            <span><i class="fas fa-envelope"></i> &nbsp; bashirshukroh359@gmail.com</span>
+          </div>
+          <div class="socials">
+            <a href="#"><i class="fab fa-facebook"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="#"><i class="fab fa-youtube"></i></a>
+          </div>
+        </div> 
+        <div class="footer-section links">
+          <h2>Quick Link</h2>
+          <br>
+          <ul>
+            <a href="#"><li>Events</li></a>
+            <a href="#"><li>Team</li></a>
+            <a href="#"><li>Mentors</li></a>
+            <a href="#"><li>Gallery</li></a>
+            <a href="#"><li>Terms and Condition</li></a>
+          </ul>
+        </div>
+        <div class="footer-section contact-form">
+          <h2>Contact us</h2>
+          <br>
+          <form action="index.html" method="post">
+            <input type="email" name="email" class="text-input contact-input" placeholder="Your email address....">
+            <textarea row="4" name="message" class="text-input contact-input" placeholder="Your message..."></textarea>
+            <button type="submit" class="btn btn-big contact-btn">
+              <i class="fas fa-envelope"></i>
+              Send
+            </button>
+          </form>
+        </div>
+      </div>
+      <div class="footer-bottom">
+        &copy; | Designed by Devshuk
+      </div>
+    </div>
+  <!-- //footer -->
+
+  <!-- JQuery -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+  <!-- Slick Carousel -->
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+
+  <!-- Custom Script -->
+   <script src="./assets/js/script.js"></script>
+</body>
+</html>
---
+++ single.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Font Awesome  -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+  <link rel="stylesheet" href="https://use.fontawesome.com/release/v5.7.2/css/all.css" crossorigin="anonymous">
+
+  <!-- Custom Css-->
+  <link rel="stylesheet" href="./assets/css/style.css">
+
+  <!-- Google fonts -->
+  <link href="https://fonts.googleapis.com/css2?family=Candal&family=Lora:ital,wght@0,400..700;1,400..700&display=swap"
+    rel="stylesheet">
+
+  <title>Single Post</title>
+</head>
+
+<body>
+  <header>
+    <div class="logo">
+      <h1 class="logo-text"><span>Dev</span>Shuk</h1>
+    </div>
+    <i class="fa fa-bars menu-toggle"></i>
+    <ul class="nav">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Services</a></li>
+      <!-- <li><a href="#">Sign up</a></li>
+      <li><a href="#">Login</a></li> -->
+      <li>
+        <a href="#">
+          <i class="fa fa-user"></i>
+          Dev Shuk
+          <i class="fa fa-chevron-down" style="font-size: .8em;"></i>
+        </a>
+        <ul>
+          <li><a href="#">Dashboard</a></li>
+          <li><a href="#" class="logout">Logout</a></li>
+        </ul>
+      </li>
+    </ul>
+  </header>
+
+  <!-- Page Wrapper -->
+  <div class="page-wrapper">
+
+  <!-- Content -->
+ <div class="content clearfix">
+
+ <!-- Main Content -->
+  <div class="main-content single">
+<h1 class="post-title">This is the title of the post</h1>
+
+<div class="post-content">
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis ut, suscipit et vel eius tenetur!</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus necessitatibus deleniti totam impedit provident, nobis sint distinctio eius exercitationem cupiditate itaque sed architecto mollitia ipsa sequi harum corporis. Ullam, inventore.</p>
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas amet totam repellat suscipit. Quidem, ab iste temporibus dolorem itaque ea doloremque delectus numquam rerum. Ipsam blanditiis at aspernatur ex, quaerat reprehenderit deleniti quia fuga rem laborum sed quisquam obcaecati ipsa!</p>
+  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium, deleniti?</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis ut, suscipit et vel eius tenetur!</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus necessitatibus deleniti totam impedit provident, nobis sint distinctio eius exercitationem cupiditate itaque sed architecto mollitia ipsa sequi harum corporis. Ullam, inventore.</p>
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas amet totam repellat suscipit. Quidem, ab iste temporibus dolorem itaque ea doloremque delectus numquam rerum. Ipsam blanditiis at aspernatur ex, quaerat reprehenderit deleniti quia fuga rem laborum sed quisquam obcaecati ipsa!</p>
+  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium, deleniti?</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis ut, suscipit et vel eius tenetur!</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus necessitatibus deleniti totam impedit provident, nobis sint distinctio eius exercitationem cupiditate itaque sed architecto mollitia ipsa sequi harum corporis. Ullam, inventore.</p>
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas amet totam repellat suscipit. Quidem, ab iste temporibus dolorem itaque ea doloremque delectus numquam rerum. Ipsam blanditiis at aspernatur ex, quaerat reprehenderit deleniti quia fuga rem laborum sed quisquam obcaecati ipsa!</p>
+  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium, deleniti?</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis ut, suscipit et vel eius tenetur!</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus necessitatibus deleniti totam impedit provident, nobis sint distinctio eius exercitationem cupiditate itaque sed architecto mollitia ipsa sequi harum corporis. Ullam, inventore.</p>
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas amet totam repellat suscipit. Quidem, ab iste temporibus dolorem itaque ea doloremque delectus numquam rerum. Ipsam blanditiis at aspernatur ex, quaerat reprehenderit deleniti quia fuga rem laborum sed quisquam obcaecati ipsa!</p>
+  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium, deleniti?</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis ut, suscipit et vel eius tenetur!</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus necessitatibus deleniti totam impedit provident, nobis sint distinctio eius exercitationem cupiditate itaque sed architecto mollitia ipsa sequi harum corporis. Ullam, inventore.</p>
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas amet totam repellat suscipit. Quidem, ab iste temporibus dolorem itaque ea doloremque delectus numquam rerum. Ipsam blanditiis at aspernatur ex, quaerat reprehenderit deleniti quia fuga rem laborum sed quisquam obcaecati ipsa!</p>
+  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium, deleniti?</p>
+</div>
+  </div>
+  <!-- // Main Content -->
+
+  <!-- Sidebar -->
+  <div class="sidebar single">
+
+  <div class="section popular">
+    <h2 class="section-title">popular</h2>
+
+    <div class="post clearfix">
+      <img src="./assets/img/1.jpg" alt="">
+      <a href="" class="title"><h4>How to overcome your fears</h4></a>
+    </div>
+
+    <div class="post clearfix">
+      <img src="./assets/img/1.jpg" alt="">
+      <a href="" class="title"><h4>How to overcome your fears</h4></a>
+    </div>
+
+    <div class="post clearfix">
+      <img src="./assets/img/1.jpg" alt="">
+      <a href="" class="title"><h4>How to overcome your fears</h4></a>
+    </div>
+
+    <div class="post clearfix">
+      <img src="./assets/img/1.jpg" alt="">
+      <a href="" class="title"><h4>How to overcome your fears</h4></a>
+    </div>
+
+    <div class="post clearfix">
+      <img src="./assets/img/1.jpg" alt="">
+      <a href="" class="title"><h4>How to overcome your fears</h4></a>
+    </div>
+
+  </div>
+    <div class="section topics">
+      <h2 class="section-title">Topicas</h2>
+      <ul>
+        <li><a href="#">Poems</a></li>
+        <li><a href="#">Qoutes</a></li>
+        <li><a href="#">Fiction</a></li>
+        <li><a href="#">Biography</a></li>
+        <li><a href="#">Motivation</a></li>
+        <li><a href="#">Inspiration</a></li>
+        <li><a href="#">Life Lessons</a></li>
+      </ul>
+    </div>
+
+  </div>
+  <!-- // sidebar -->
+ </div>
+  <!--// Content -->
+
+  </div>
+  <!-- end of page-wrapper -->
+
+  <!-- footer -->
+    <div class="footer">
+      <div class="footer-content">
+        <div class="footer-section about">
+          <h1 class="logo-text"><span>Dev</span>Shuk</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus optio possimus iure ex molestiae nihil vero, rerum, dolorum nobis.</p>
+          <div class="contact">
+            <span><i class="fas fa-phone"></i> &nbsp; 7066896092</span>
+            <span><i class="fas fa-envelope"></i> &nbsp; bashirshukroh359@gmail.com</span>
+          </div>
+          <div class="socials">
+            <a href="#"><i class="fab fa-facebook"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="#"><i class="fab fa-youtube"></i></a>
+          </div>
+        </div> 
+        <div class="footer-section links">
+          <h2>Quick Link</h2>
+          <br>
+          <ul>
+            <a href="#"><li>Events</li></a>
+            <a href="#"><li>Team</li></a>
+            <a href="#"><li>Mentors</li></a>
+            <a href="#"><li>Gallery</li></a>
+            <a href="#"><li>Terms and Condition</li></a>
+          </ul>
+        </div>
+        <div class="footer-section contact-form">
+          <h2>Contact us</h2>
+          <br>
+          <form action="index.html" method="post">
+            <input type="email" name="email" class="text-input contact-input" placeholder="Your email address....">
+            <textarea row="4" name="message" class="text-input contact-input" placeholder="Your message..."></textarea>
+            <button type="submit" class="btn btn-big contact-btn">
+              <i class="fas fa-envelope"></i>
+              Send
+            </button>
+          </form>
+        </div>
+      </div>
+      <div class="footer-bottom">
+        &copy; | Designed by Devshuk
+      </div>
+    </div>
+  <!-- //footer -->
+
+  <!-- JQuery -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+  <!-- Slick Carousel -->
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+
+  <!-- Custom Script -->
+   <script src="./assets/js/script.js"></script>
+</body>
+</html>
